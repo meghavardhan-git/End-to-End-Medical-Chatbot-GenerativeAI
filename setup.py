38d7dc8aba524d12a265ac04e5ea0e8484a6fd0a
meghavardhan-git <meghanani122005@gmail.com>
2025-09-28 19:34:12 +0530

    medibotupdated

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'End to End Medica chat bot',
+    version= '0.0.0',
+    author= 'megha vardhan',
+    author_email= 'meghavardhan2212@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
