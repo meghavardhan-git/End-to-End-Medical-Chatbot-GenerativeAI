1862f108aa762d4c3706cb567d5914a10e12d5c5
meghavardhan-git <meghanani122005@gmail.com>
2025-10-01 15:06:06 +0530

    cicd added

--- app.py
+++ app.py
@@ -62,8 +62,5 @@ def chat():
 
 
 
-if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
-
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port= 8080, debug=False)
